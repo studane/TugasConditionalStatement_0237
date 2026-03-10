f4145d3919131839da2c51ad2136d8d20f2bb903
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-10 11:24:55 +0700

    hitungbmi

--- file1.cpp
+++ file1.cpp
@@ -7,3 +7,7 @@ void inputData(float &berat, float &tinggi){
     cout <<"Masukkan tinggi badan (meter):";
     cin >> tinggi;
 }
+
+float hitungBMI(float berat, float tinggi){
+    return berat / (tinggi * tinggi);
+}
