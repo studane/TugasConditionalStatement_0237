27bb6eb773a49087d17e8d0e9536330afeb8c65e
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-10 11:38:57 +0700

    finish

--- file1.cpp
+++ file1.cpp
@@ -32,4 +32,10 @@ int main(){
 
     cout << "---Selamat Datang Mahasiswa Kelas A 2025---" << endl;
     inputData(berat, tinggi);
+    bmi = hitungBMI(berat, tinggi);
+
+    cout << endl;
+    cout << "---Hasil" <<endl;
+    cout << "BMI Anda:" <<bmi <<endl;
+    cout << "Status :" << statusBMI(bmi) <<endl;
 }
