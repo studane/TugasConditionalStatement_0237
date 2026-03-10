713bdef79a934f221f40428421a1ec21bd085b2f
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-10 11:22:07 +0700

    inputdata

---
+++ file1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void inputData(float &berat, float &tinggi){
+    cout <<"Masukkan berat badan (kg):";
+    cin >> berat;
+    cout <<"Masukkan tinggi badan (meter):";
+    cin >> tinggi;
+}
